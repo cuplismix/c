33442f476b8fd76e2c2a56b664942a2bc9b5158e
cuplismix <40352673+cuplismix@users.noreply.github.com>
2018-07-23 11:11:40 +0800

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -28,10 +28,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '8.0.2'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'Nadya'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_VER  = '2.1.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
